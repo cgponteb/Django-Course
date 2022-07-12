b5af872e3b9585b30f6116e4a61e22aaf6414c70
Carlos Guillermo Ponte Babarro <cgponteb@proton.me>
2022-07-12 16:35:22 +0200

    Practice with parameters in URL

--- holamundo/holamundo/urls.py
+++ holamundo/holamundo/urls.py
@@ -20,5 +20,6 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('saludo/', views.saludo, name='saludo'),
-    path('despedida/', views.despedida, name='despedida')
+    path('despedida/', views.despedida, name='despedida'),
+    path('adulto/<int:edad>', views.adulto, name='adulto'),
 ]
--- holamundo/holamundo/views.py
+++ holamundo/holamundo/views.py
@@ -5,3 +5,9 @@ def saludo(request):
 
 def despedida(request):
   return HttpResponse("Hasta pronto!")
+
+def adulto(request, edad):
+  if edad >= 18:
+    return HttpResponse("Eres mayor de edad")
+  else:
+    return HttpResponse("Eres menor de edad")
